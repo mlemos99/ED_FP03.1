85c412e9185403df5f73d3be0a101be7fb73ec62
mlemos99 <44319976+mlemos99@users.noreply.github.com>
2019-10-18 10:19:14 +0100

    Second version

---
+++ src/Exercicio1/EmptyCollectionException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicio1;
+
+/**
+ *
+ * @author Utilizador
+ */
+class EmptyCollectionException extends Exception {
+    public EmptyCollectionException(){
+        super();
+    }
+    
+    public EmptyCollectionException(String message) {
+        super(message);
+    }
+}
